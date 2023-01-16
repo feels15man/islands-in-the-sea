eefb7ae181ff57b95fe027dc912a79d78d0fea9a
feels15man <ilya.shabanov2004@mail.ru>
2023-01-16 03:02:35 +0300

    v1.1.1 fixed + work

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
-#define N 8
+#define N 9
 
 int k = 0;
-char map[8][9], ch_hor[N][N + 1], ch_ver[N][N + 1];
+char map[N][9], ch_hor[N][N + 1], ch_ver[N][N + 1];
 char ch_hor_tmp[N][N + 1], ch_ver_tmp[N][N + 1];
 int int_hor[N][N] = {0}, int_ver[N][N] = {0};
 int n, c_pr = 0;
 
-void str_to_int(const char str[], int res[8]) {
+void str_to_int(const char str[], int res[N]) {
     int counter = 0;
     int iter = 0;
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < N; i++) {
         if (str[i] == '*') counter++;
         else if (counter != 0) {
             res[iter] = counter;
@@ -26,7 +27,7 @@ int check_map() {
 //    map всегда один
     for (int i = 0; i < n; i++)
     {
-        int tmp_arr[8] = {0};
+        int tmp_arr[N] = {0};
         str_to_int(map[i], tmp_arr);
         for (int j = 0; j < n; j++)
         {
@@ -37,7 +38,7 @@ int check_map() {
     for (int i = 0; i < n; i++)
     {
         char tmp_str[9] = {"00000000"};
-        int tmp_int[8] = {0};
+        int tmp_int[N] = {0};
         for (int j = 0; j < n; j++){
             tmp_str[j] = map[j][i];
         }
@@ -53,8 +54,8 @@ int check_map() {
 
 void cut1(char str[]) {
     int ind, i = -1;
-    while(str[++i] != '0');
-    if (i == 8) str[8] = 0;
+    while(str[++i] != '0' && i < N);
+    if (i == N) str[7] = '0';
     else {
         ind = strstr(str, "0") - str;
         str[ind - 1] = '0';
@@ -64,15 +65,13 @@ void cut1(char str[]) {
 void add(char str[], char sym) {
     if (strstr(str, "0") != NULL) str[strstr(str, "0") - str] = sym;
     else {
-        int res[8];
+        int res[N];
         str_to_int(str, res);
         if (res[0] == 0) {
             str[0] = '*';
             return;
         }
-        int i = 0;
-        while (str[i] != '_' || str[i + 1] != '_') i++;
-        str[i + 1] = sym;
+        str[7] = '0';
     }
 }
 
@@ -107,11 +106,11 @@ void set_arr(char arr[][9], char sym) {
 int islands_str_cmp(char str1[], char str2[]) {
     //str2 - начало str1
 //    int flag_res = 1;
-    int arr1[8] = {0}, arr2[8] = {0};
+    int arr1[N] = {0}, arr2[N] = {0};
     str_to_int(str1, arr1);
     str_to_int(str2, arr2);
 //    if (arr2[0] == 0) return 0;
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < n; i++) {
         if (arr1[i] < arr2[i] && arr2[i]/*arr1[i] != arr2[i]*/) return 0/*return 0*/;
     }
     return 1;
@@ -120,38 +119,31 @@ int islands_str_cmp(char str1[], char str2[]) {
 int islands_str_cmp_strogo(char str1[], char str2[]) {
     //str2 - начало str1
     int iter1 = 0, iter2 = 0;
-    int arr1[8] = { 0 }, arr2[8] = { 0 };
+    int arr1[N] = { 0 }, arr2[N] = { 0 };
     str_to_int(str1, arr1);
     str_to_int(str2, arr2);
     /*if (arr2[0] == 0) return 0;*/
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < n; i++) {
         if (arr1[i] != arr2[i]) return 0;
     }
     return 1;
 }
 
-void clean_zero(char arr[][9]) {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (arr[i][j] == '0') arr[i][j] = '_';
-        }
-    }
-}
 
 void recursion(int i, int j) {
     k++;
-    printf("%d %d\n", i, j);
+//    printf("%d %d\n", i, j);
 //    print_arr(map);
     if (i == n) {
-        printf("---------------------------\n");
-        printf("%d - %d kogda i > n\n",i,j);
-        print_arr(map);
-        printf("\n");
-        print_arr(ch_hor_tmp);
-        printf("\n");
-        print_arr(ch_ver_tmp);
-        printf("\n");
-        printf("---------------------------\n");
+//        printf("---------------------------\n");
+//        printf("%d - %d kogda i > n\n",i,j);
+//        print_arr(map);
+//        printf("\n");
+//        print_arr(ch_hor_tmp);
+//        printf("\n");
+//        print_arr(ch_ver_tmp);
+//        printf("\n");
+//        printf("---------------------------\n");
         if (check_map()) {
             put_map();
             c_pr = 1;
@@ -177,11 +169,11 @@ void recursion(int i, int j) {
         map[i][j] = '_';
 
         int flag_i, flag_j;
-        flag_i = (j > 0) && map[i][j - 1] == '*';
+        flag_i = (j > 0) && map[i][j - 1] == '*' ;
         flag_j = (i > 0) && map[i - 1][j] == '*';
 
-        if (flag_i) add(ch_hor_tmp[i], '_');
-        if (flag_j) add(ch_ver_tmp[j], '_');
+        if (flag_i && j != N) add(ch_hor_tmp[i], '_');
+        if (flag_j && i != N) add(ch_ver_tmp[j], '_');
 
         if (islands_str_cmp(ch_hor[i], ch_hor_tmp[i])
             && islands_str_cmp(ch_ver[j], ch_ver_tmp[j])) {
@@ -195,6 +187,10 @@ void recursion(int i, int j) {
 
 
 int main() {
+
+    double time_spent = 0.0;
+    clock_t begin = clock();
+
     FILE *input;
     FILE *output = fopen("../output.txt", "w");
     fclose(output);
@@ -221,8 +217,6 @@ int main() {
 //        printf("\n");
     }
 
-//
-//    printf("\n");
 
     //vertical
     for (int i = 0; i < n; i++) {
@@ -238,17 +232,7 @@ int main() {
 //        printf("\n");
     }
 
-//    clean_zero(ch_hor);
-//    clean_zero(ch_ver);
-//    clean_zero(ch_hor_tmp);
-//    clean_zero(ch_ver_tmp);
-//    clean_zero(map);
-//    for (int i = 0; i < n; i++){
-//        for (int j = 0; j < n; j++){
-//            printf("%d ", int_ver[i][j]);
-//        }
-//        printf("\n");
-//    }
+
     recursion(0, 0);
     fclose(input);
     if (!c_pr) {
@@ -256,6 +240,9 @@ int main() {
         fputs("no map", output);
         fclose(output);
     }
-    printf("%d", k);
+
+    time_spent += (double)(clock() - begin) / CLOCKS_PER_SEC;
+
+    printf("%d time:%f", k, time_spent);
     return 0;
 }
